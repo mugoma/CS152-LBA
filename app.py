2d90b9e08b2549d1a286d5022474ba0f263d34ae
Okomba Mugoma <mugoma99@gmail.com>
2025-04-19 14:07:28 -0700

    Update.

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
 # create a flask app
-from pathlib import Path
-from flask import Blueprint, Flask, request, jsonify, render_template
-from flask_sockets import Sockets
+from flask import Flask, request, session, render_template
 from flask_cors import CORS
 from pyswip.prolog import Prolog
 from pyswip.easy import *
 
-import sys
 from flask_socketio import SocketIO, emit
 import threading
 import uuid
 
-#TODO: Refactor/clean up the code 
-
-# socketio = SocketIO()
-# bp = Blueprint("index", __name__,template_folder='.')
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.register_blueprint(bp)
-#     socketio.init_app(app)
-#     return app
+# TODO: Refactor/clean up the code
 
 # Initialize Flask app
 app = Flask(__name__, template_folder=".")
@@ -28,11 +16,36 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # Enable CORS for all routes
 CORS(app)
 
+import pyswip, ctypes
 
-from pyswip.prolog import Prolog
-from pyswip.easy import *
+class PrologMT(pyswip.Prolog):
+    """Multi-threaded (one-to-one) pyswip.Prolog ad-hoc reimpl"""
+    _swipl = pyswip.core._lib
 
-prolog = Prolog()  # Global handle to interpreter
+    PL_thread_self = _swipl.PL_thread_self
+    PL_thread_self.restype = ctypes.c_int
+
+    PL_thread_attach_engine = _swipl.PL_thread_attach_engine
+    PL_thread_attach_engine.argtypes = [ctypes.c_void_p]
+    PL_thread_attach_engine.restype = ctypes.c_int
+
+    @classmethod
+    def _init_prolog_thread(cls):
+        pengine_id = cls.PL_thread_self()
+        if (pengine_id == -1):
+            pengine_id = cls.PL_thread_attach_engine(None)
+            print("{INFO} attach pengine to thread: %d" % pengine_id)
+        if (pengine_id == -1):
+            raise pyswip.prolog.PrologError("Unable to attach new Prolog engine to the thread")
+        elif (pengine_id == -2):
+            print("{WARN} Single-threaded swipl build, beware!")
+
+    class _QueryWrapper(pyswip.Prolog._QueryWrapper):
+        def __call__(self, *args, **kwargs):
+            PrologMT._init_prolog_thread()
+            return super().__call__(*args, **kwargs)
+
+prolog = PrologMT()  # Global handle to interpreter
 
 retractall = Functor("retractall")
 known = Functor("known", 3)
@@ -41,20 +54,13 @@ known = Functor("known", 3)
 pending_inputs = {}
 
 
-# Define foreign functions for getting user input and writing to the screen
-def write_py(X):
-    print(str(X))
-    sys.stdout.flush()
-    return True
-
-
-def read_py(A, V, Y):
+def get_user_response(A, V, Y, P: str):
     if isinstance(Y, Variable):
         req_id = str(uuid.uuid4())
         event = threading.Event()
         pending_inputs[req_id] = {"event": event, "answer": None}
         # Emit a SocketIO event to the frontend (from the main thread)
-        socketio.emit("input_request", {"question": f"{A} is {V}?", "req_id": req_id})
+        socketio.emit("input_request", {"question": str(P).format(V), "req_id": req_id})
         # Wait for the answer to be set by the handler
         event.wait()
         answer = pending_inputs[req_id]["answer"]
@@ -65,11 +71,9 @@ def read_py(A, V, Y):
         return False
 
 
-write_py.arity = 1
-read_py.arity = 3
+get_user_response.arity = 4
 
-registerForeign(read_py)
-registerForeign(write_py)
+registerForeign(get_user_response)
 
 
 # Define routes
@@ -85,19 +89,28 @@ def handle_connect():
 
 @socketio.on("start")
 def handle_start():
-    print("Starting Session")
-    prolog.consult("expert_system.pl", relative_to=__file__)
-    call(retractall(known))
-    query = prolog.query("problem(X).", maxresult=1)
     try:
+        # Check if session is new.If not, terminate the existing prolog thread
+        if session.get("prolog_thread"):
+            session["prolog_thread"] = ''
+
+        # session["prolog_thread"] = threading.Thread(target=prolog_thread)
+
+        print("Starting Session")
+        prolog.consult("cafe_recommender.pl", relative_to=__file__)
+        # prolog.consult("expert_system.pl", relative_to=__file__)
+        call(retractall(known))
+        query = prolog.query("cafe(X).", maxresult=1)
+        # query = prolog.query("problem(X).", maxresult=1)
+
         problem = next(query)
     except Exception as e:
         print(f"Error: {e}")
         problem = None
     if problem:
-        response = f"Problem is {problem["X"]}"
+        response = f"Recommended cafe is {problem['X']}"
     else:
-        response = "No problem identified."
+        response = "No cafe identified."
     emit("final_problem", {"problem": response})
     print("Ending Session")
 
@@ -105,20 +118,6 @@ def handle_start():
 @socketio.on("disconnect")
 def handle_disconnect():
     print("Client disconnected")
-
-# TODO: Remove function
-@socketio.on("message")
-def handle_message(message):
-    call(retractall(known))
-    # Process the message and generate a response
-    problem = prolog.query(message, maxresult=1)
-    # problem = [s for s in prolog.query("problem(X).", maxresult=1)]
-    next_query = next(problem)
-    if next_query:
-        response = next_query
-    else:
-        response = "No problem identified."
-    emit("response", response)
 
 
 # SocketIO handler for user input
@@ -132,5 +131,4 @@ def handle_user_input(data):
 
 
 if __name__ == "__main__":
-    # socketio.run(app, host
     socketio.run(app, debug=True)
---
+++ cafe_recommender.pl
@@ -0,0 +1,91 @@
+%  Tell prolog that known/3 and multivalued/1 will be added later
+:- dynamic known/3, multivalued/1.
+
+% Enter your KB below this line:
+
+cafe('Cafe Buena Vida'):- noise(quiet), neighborhood(mission), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegan), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Diamond Coffee N’ Pastry'):- noise(moderate), neighborhood(outer_sunset), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Beluna Cafe'):- noise(quiet), neighborhood(lower_haight), price('$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.9), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Delah Coffee'):- noise(moderate), neighborhood(soma), price('$'), ownership(local), least_busy(evening), dietary_preference(none), rating(4.7), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Home Coffee Roasters'):- noise(moderate), neighborhood(outer_sunset), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('The Social Study'):- noise(noisy), neighborhood(western_addition), price('$$'), ownership(local), least_busy(evening), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Cafe Réveille'):- noise(quiet), neighborhood(lower_haight), price('$$'), ownership(chain), least_busy(morning), dietary_preference(vegetarian), rating(4.3), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Mercury Cafe'):- noise(quiet), neighborhood(hayes_valley), price('$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.3), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Java Beach Cafe'):- noise(noisy), neighborhood(outer_sunset), price('$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.4), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Progressive Grounds'):- noise(quiet), neighborhood(bernal_heights), price('$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Le Café du Soleil'):- noise(quiet), neighborhood(lower_haight), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Blue Bottle Hayes Valley'):- noise(quiet), neighborhood(hayes_valley), price('$$'), ownership(chain), least_busy(morning), dietary_preference(gluten_free), rating(4.6), has_wifi(yes), \+has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Cafe La Flore'):- noise(quiet), neighborhood(inner_sunset), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Cafe International'):- noise(moderate), neighborhood(lower_haight), price('$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Fiddle Fig Cafe'):- noise(moderate), neighborhood(russian_hill), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Ocean Beach Cafe'):- noise(noisy), neighborhood(outer_richmond), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.7), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Royal Ground Coffee'):- noise(noisy), neighborhood(nob_hill), price('$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.4), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Manny’s'):- noise(quiet), neighborhood(mission), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Simple Pleasures Cafe'):- noise(quiet), neighborhood(outer_richmond), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Philz Coffee Mission'):- noise(noisy), neighborhood(mission), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Equator Coffees Presidio'):- noise(moderate), neighborhood(presidio), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(3.9), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Saint Frank Coffee Russian Hill'):- noise(quiet), neighborhood(russian_hill), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Sightglass Coffee'):- noise(quiet), neighborhood(mission), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Andytown Coffee Roasters'):- noise(quiet), neighborhood(outer_sunset), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Ritual Coffee Roasters'):- noise(moderate), neighborhood(hayes_valley), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Diamond Coffee N’ Pastry'):- noise(moderate), neighborhood(outer_sunset), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Beluna Cafe'):- noise(quiet), neighborhood(lower_haight), price('$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.9), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Delah Coffee'):- noise(moderate), neighborhood(soma), price('$'), ownership(local), least_busy(evening), dietary_preference(none), rating(4.7), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Home Coffee Roasters'):- noise(moderate), neighborhood(outer_sunset), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('The Social Study'):- noise(noisy), neighborhood(western_addition), price('$$'), ownership(local), least_busy(evening), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Cafe Réveille'):- noise(quiet), neighborhood(lower_haight), price('$$'), ownership(chain), least_busy(morning), dietary_preference(vegetarian), rating(4.3), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Mercury Cafe'):- noise(quiet), neighborhood(hayes_valley), price('$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.3), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Java Beach Cafe'):- noise(noisy), neighborhood(outer_sunset), price('$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.4), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Progressive Grounds'):- noise(quiet), neighborhood(bernal_heights), price('$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Le Café du Soleil'):- noise(quiet), neighborhood(lower_haight), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Blue Bottle Hayes Valley'):- noise(quiet), neighborhood(hayes_valley), price('$$'), ownership(chain), least_busy(morning), dietary_preference(gluten_free), rating(4.6), has_wifi(yes), \+has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Cafe La Flore'):- noise(quiet), neighborhood(inner_sunset), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Cafe International'):- noise(moderate), neighborhood(lower_haight), price('$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Fiddle Fig Cafe'):- noise(moderate), neighborhood(russian_hill), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Ocean Beach Cafe'):- noise(noisy), neighborhood(outer_richmond), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.7), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Royal Ground Coffee'):- noise(noisy), neighborhood(nob_hill), price('$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.4), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Mannys'):- noise(quiet), neighborhood(mission), price('$$'), ownership(local), least_busy(morning), dietary_preference(vegetarian), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Simple Pleasures Cafe'):- noise(quiet), neighborhood(outer_richmond), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Philz Coffee Mission'):- noise(noisy), neighborhood(mission), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Equator Coffees Presidio'):- noise(moderate), neighborhood(presidio), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(3.9), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Saint Frank Coffee Russian Hill'):- noise(quiet), neighborhood(russian_hill), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Sightglass Coffee'):- noise(quiet), neighborhood(mission), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Andytown Coffee Roasters'):- noise(quiet), neighborhood(outer_sunset), price('$$'), ownership(chain), least_busy(morning), dietary_preference(none), rating(4.6), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+cafe('Ritual Coffee Roasters'):- noise(moderate), neighborhood(hayes_valley), price('$$'), ownership(local), least_busy(morning), dietary_preference(none), rating(4.5), has_wifi(yes), has_plugs(yes), is_lgbt_friendly(yes).
+
+
+% The code below implements the prompting to ask the user:
+
+noise(X) :- ask(noise, X, 'Is your preferred noise level: {0} ?').
+neighborhood(X) :- ask(neighborhood, X, 'Do you want a cafe in "{0}" neighborhood?').
+price(X) :- ask(price, X, 'Do you prefer a cafe that has {0} price rating on Google Maps?').
+ownership(X) :- ask(ownership, X, 'Do you prefer a cafe that is {0} owned?').
+least_busy(X) :- ask(least_busy, X, 'Do you prefer going to the cafe when it is least busy in the {0}?').
+dietary_preference(X) :- ask(dietary_preference, X, 'Do you want a cafe that caters to this dietary restriction:{0}?').
+rating(X) :- ask(rating, X, 'Is the following rating good enough {0}?').
+has_wifi(X) :- ask(has_wifi, X, 'Do you want a cafe that responded "{0}" to having Wifi?').
+has_plugs(X) :- ask(has_plugs, X, 'Do you want a cafe that responded "{0}" to having plugs?').
+is_lgbt_friendly(X) :- ask(is_lgbt_friendly, X, 'Do you want a cafe that responded "{0}" to  being LGBT-friendly:').
+
+
+% Asking clauses
+
+ask(A, V, _):-
+known(yes, A, V), % succeed if true
+!.	% stop looking
+
+ask(A, V, _):-
+known(_, A, V), % fail if false
+!, fail.
+
+% If not multivalued, and already known to be something else, don't ask again for a different value.
+ask(A, V, _):-
+\+multivalued(A),
+known(yes, A, V2),
+V \== V2,
+!, fail.
+
+ask(A, V, P):-
+get_user_response(A,V,Y,P), % get the answer
+assertz(known(Y, A, V)), % remember it
+Y == yes.	% succeed or fail
